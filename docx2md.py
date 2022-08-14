c0cfd3e70cf0d29d176bb554e26e04d8ded8638f
liantian <liantian.me+code@gmail.com>
2022-08-14 12:20:26 +0800

    fix

--- docx2md.py
+++ docx2md.py
@@ -2,32 +2,31 @@
 # -*- coding: utf-8 -*-
 
 from typing import Union
-from slugify import slugify
+from hashlib import md5
 from pathlib import Path, PurePosixPath
 from zipfile import ZipFile
 from PIL import Image
 from io import BytesIO
-from docx import Document
 from subprocess import Popen
-from datetime import datetime
 
 __all__ = ['docx2md']
 
 
-def sava_image(file_path_name: str, archive: ZipFile, save_dir: Path, sub_dir: PurePosixPath, name_prefix: str):
-    origin_filename = PurePosixPath(file_path_name).name
-
-    file_name = name_prefix + '-' + PurePosixPath(file_path_name).stem + ".png"
-    save_path = sub_dir.joinpath(file_name)
-    final_save_path = save_dir.joinpath(save_path)
+def sava_image(file_path_name: str, archive: ZipFile, save_dir: Path, sub_dir: PurePosixPath):
 
     img_data = archive.read(file_path_name)
     fh = BytesIO(img_data)
-    img = Image.open(fh)
-    final_save_path.parent.mkdir(parents=True, exist_ok=True)
-    img.save(final_save_path, "PNG")
 
-    return origin_filename, save_path
+    md5sum = md5(img_data).hexdigest()
+    output_file_name = md5sum + '.png'
+    output_file_path = sub_dir.joinpath(output_file_name)
+    output_file_full_path = save_dir.joinpath(output_file_path)
+
+    img = Image.open(fh)
+    output_file_full_path.parent.mkdir(parents=True, exist_ok=True)
+    img.save(output_file_full_path, "PNG")
+
+    return PurePosixPath(file_path_name).name, output_file_path
 
 
 def docx2md(src_file: Union[Path, str]):
@@ -40,18 +39,12 @@ def docx2md(src_file: Union[Path, str]):
     elif src_file.suffix != '.docx':
         raise ValueError("{} is not a docx file".format(src_file))
 
-    document = Document(src_file)
     src_dir = src_file.parent
-    slug = slugify(src_file.stem)
     doc_filename = src_file.stem
+    # slug = slugify(src_file.stem)
 
-    output_rst_file_name = slug + ".md"
+    output_rst_file_name = doc_filename + ".md"
     output_rst_file_path = src_dir.joinpath(output_rst_file_name)
-
-    try:
-        created = document.core_properties.created
-    except:
-        created = datetime.now()
 
     img_save_dir = PurePosixPath(".").joinpath("img")
 
@@ -65,8 +58,7 @@ def docx2md(src_file: Union[Path, str]):
                     file_path_name=x,
                     archive=archive,
                     save_dir=src_dir,
-                    sub_dir=img_save_dir,
-                    name_prefix=slug)
+                    sub_dir=img_save_dir)
                 img_list.append((origin_filename, save_path.__str__()))
             except:
                 pass
